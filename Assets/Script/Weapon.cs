33da77157e47cabbde0ea64100e4a9867e6405cb
anujrocks <anujsahabest0111@gmail.com>
2024-10-16 02:00:00 +0530

    Weapon Animator

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -1,37 +1,72 @@
-
+using System.Collections;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
-    private Animator animator;
 
+    public Animator weaponAnimator;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
     // Update is called once per frame
     void Update()
     {
+        if (isReloading)
+            return;
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetButtonDown("Fire1"))
         {
-            animator = GetComponent<Animator>();
             Shoot();
         }
     }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        weaponAnimator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        weaponAnimator.SetBool("Reloading", false);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
     void Shoot()
     {
+        if (isReloading)
+            return;
+
         muzzleFlash.Play();
-        animator.SetTrigger("recoil");
+        currentAmmo--;
+
         RaycastHit hit;
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
-           Enemy target = hit.transform.GetComponent<Enemy>();
-            if (target != null) 
+            Enemy target = hit.transform.GetComponent<Enemy>();
+            if (target != null)
             {
                 target.takeDamage(damage);
             }
         }
-
     }
 }
